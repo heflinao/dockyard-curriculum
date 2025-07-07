4efaebeb476b614525c14c6e9a478900de3adbe4
Alex Heflin <heflin.alexanderia@gmail.com>
2025-07-07 14:58:40 -0400

    Create CardCounting module

---
+++ card_counting.exs
@@ -0,0 +1,9 @@
+defmodule CardCounting do
+	def find_card_count(new_card_number) do
+		_high = [10, 11, 12, 13]
+		_normal = [7, 8, 9]
+		_low = [1, 2, 3, 4, 5, 6]
+
+		_initial_count = 0
+	end
+end
