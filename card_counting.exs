35106c0c3e6a984ed45f8375677f7a168bb4633f
Alex Heflin <heflin.alexanderia@gmail.com>
2025-07-08 12:01:29 -0400

    Count cards based on a single card input

--- card_counting.exs
+++ card_counting.exs
@@ -1,19 +1,28 @@
 defmodule CardCounting do
 	def find_card_count(new_card_number) do
-		high = [10, 11, 12, 13]
-		normal = [7, 8, 9]
-		low = [1, 2, 3, 4, 5, 6]
+		high_cards = [10, 11, 12, 13]
+		normal_cards = [7, 8, 9]
+		low_cards = [1, 2, 3, 4, 5, 6]
 
-		_initial_count = 0
+		high = 1
+		low = -1
+		normal = 0
+
+		initial_count = 0
 
 		cond do
-			Enum.any?(high, fn card_number -> card_number == new_card_number end) -> 
+			Enum.any?(high_cards, fn card_number -> card_number == new_card_number end) ->
 				IO.puts "high: #{new_card_number}"
-			Enum.any?(normal, fn card_number -> card_number == new_card_number end) -> 
-				IO.puts "normal: #{new_card_number}"
-			Enum.any?(low, fn card_number -> card_number == new_card_number end) -> 
+				initial_count + high
+			Enum.any?(low_cards, fn card_number -> card_number == new_card_number end) ->
 				IO.puts "low: #{new_card_number}"
-			true -> IO.puts "didn't match"
+				initial_count + low
+			Enum.any?(normal_cards, fn card_number -> card_number == new_card_number end) ->
+				IO.puts "normal: #{new_card_number}"
+				initial_count + normal
+			true -> 
+				IO.puts "didn't match"
+				initial_count
 		end
 	end
 end
