0248e528a354cfc84e8ce3df8ad78c4730e5ddaf
Alex Heflin <heflin.alexanderia@gmail.com>
2025-07-10 15:21:37 -0400

    Calculate percentage of goal completion

--- habit_tracker.exs
+++ habit_tracker.exs
@@ -3,6 +3,10 @@ defmodule HabitTracker do
 	@medium 20
 	@large 30
 
+	def calculate_progress_percentage(point_goal, number_of_small_tasks, number_of_medium_tasks, number_of_large_tasks) do
+		(calculate_total_points(number_of_small_tasks, number_of_medium_tasks, number_of_large_tasks) / point_goal) * 100
+	end
+
 	def calculate_total_points(number_of_small_tasks, number_of_medium_tasks, number_of_large_tasks) do
 		number_of_small_tasks * @small + number_of_medium_tasks * @medium + number_of_large_tasks * @large
 	end
